397c0eb554ef6f0b54ae3192ae5d1ff5324bf7fd
alexisjcarr <acarr59@gmail.com>
2019-08-06 16:56:16 -0500

    ➕ Adds nextjs boilerplate and some components.
     #1

---
+++ sick-fits/frontend/components/Nav.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+const Nav = () => (
+  <div>
+    <Link>
+      <Link href="/index">
+        <a>Home</a>
+      </Link>
+    </Link>
+  </div>
+);
+
+export default Nav;
